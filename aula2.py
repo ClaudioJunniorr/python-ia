ab5ee0c28c434618965b230263ed0cc7085e4e90
ClaudioJunniorr <ClaudioJunior344@hotmail.com>
2024-07-02 11:40:36 -0300

    exercicio das somas

--- aula2.py
+++ aula2.py
@@ -9,11 +9,19 @@ Converter a idade para reeber somente numeros inteiros
 imprimir os tipos de dados
 imprimir todas as informações concatenadas usando f string
 '''
-#number=30
-#number=str(number)
+number=30
+number=str(number)
 # conversão direta de variável. Converteu int para string
-nome=input(" Digite seu nome: ")
+""" nome=input(" Digite seu nome: ")
 idade=int(input(" Sua idade: "))
 peso=float(input(" Seu peso: "))
 altura=float(input(" Sua altura: "))
-print(f" Meu nome é {nome}, tenho {idade} anos de idade, estou pesando {peso} quilos e tenho {altura}")
+print(f" Meu nome é {nome}, tenho {idade} anos de idade, estou pesando {peso} quilos e tenho {altura}") """
+num1 = int(input(" Digite o primeiro numero: "))
+num2 = int(input(" Digite o segundo numero: "))
+soma = num1+num2
+subracao = num1-num2
+divisao = num1/num2
+print(f" A soma dos numeros {num1}+{num2} é igual a : {soma}")
+print(f" A subtração dos numeros {num1}+{num2} é igual a : {subracao}")
+print(f" A divisao dos numeros {num1}+{num2} é igual a : {divisao}")
